39d1c42e090562be41d2236de3c648a665f6bee3
Deni Priyanto <denpri26@gmail.com>
2024-12-10 20:12:37 +0700

    add fungsionality filter question key by word

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import speech_recognition as sr
-import pyttsx3
 
-speaker = pyttsx3.init()
 recognizer = sr.Recognizer()
 
-# Menampilkan daftar semua perangkat input audio
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print(f"{index}: {name}")
+def answer(question, response):
+    question = question.lower()
+    
+    for key, info in response.items():
+        if key.lower() in question:
+            character = info.get("karakter", "Karakter tidak diketahui")
+            answer = info.get("jawaban", "Jawaban tidak diketahui")
+            return (f"Karakter: {character}\nJawaban: {answer}")
+    
+    return "Maaf tidak ada respon yang cocok"
 
-try:
+def speech(response):
     with sr.Microphone() as source:
-        speaker.say("Hello, Im Pipilaka. Silahkan tanya tentang aku.")
-        speaker.runAndWait()
-
         print("Silahkan Bicara...")
 
         recognizer.adjust_for_ambient_noise(source)
@@ -20,11 +22,23 @@ try:
         audio = recognizer.listen(source)
         
         try:
-            text = recognizer.recognize_google(audio)
+            text = recognizer.recognize_google(audio, language="id-ID")
             print("Anda Mengatakan: ", text)
+            
+            hasil = answer(text, response)
+            print(hasil)
+            
         except sr.UnknownValueError:
             print("Maaf, tidak dapat mengenali ucapan.")
         except sr.RequestError as e:
-            print(f"Error pada layanan: {e}")
-except Exception as e:
-    print(f"Terjadi kesalahan saat mendengarkan: {e}")
+            print(f"Error pada layanan: {e}") 
+
+if __name__ == "__main__":
+    response_data = {
+        "halo": {"karakter": "Pipilaka", "jawaban": "Halo saya Pipilaka! Ada yang bisa saya bantu?"},
+        "hai": {"karakter": "Pipilaka", "jawaban": "Halo saya Pipilaka! Ada yang bisa saya bantu?"},
+        "selamat pagi": {"karakter": "Tari", "jawaban": "Selamat pagi! Semoga harimu menyenangkan!"},
+        "terima kasih": {"karakter": "pipilaka_thankyou", "jawaban": "Sama-sama! Senang bisa membantu."}
+    }
+    
+    speech(response_data)
